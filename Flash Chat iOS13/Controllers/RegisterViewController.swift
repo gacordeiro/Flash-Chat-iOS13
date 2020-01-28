29c1f6cbe400e6e9e9c038a85c71c1542a234dad
Guilherme Cordeiro <gacordeiro@gmail.com>
2020-01-28 18:45:04 -0300

    Adding Firebase

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        errorMessageLabel.text = ""
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorMessageLabel.text = e.localizedDescription
+                } else {
+                    self.performSegue(withIdentifier: "registerToChat", sender: self)
+                }
+            }
+        }
     }
     
 }

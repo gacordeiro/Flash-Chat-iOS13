44a9faf6b6ed1709dff9c9aac2ca4d3dfe8efd64
Guilherme Cordeiro <gacordeiro@gmail.com>
2020-01-29 08:17:05 -0300

    sign in and sign out lesson

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        errorMessageLabel.text = ""
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorMessageLabel.text = e.localizedDescription
+                } else {
+                    self.performSegue(withIdentifier: "loginToChat", sender: self)
+                }
+            }
+        }
     }
     
 }

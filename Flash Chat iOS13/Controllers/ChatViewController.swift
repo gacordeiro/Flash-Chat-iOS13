c806f2805b38a2a4708717976a37084c6baa07ab
Guilherme Cordeiro <guilherme.cordeiro@luizalabs.com>
2020-01-30 16:24:48 -0300

    layout adjustments

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -16,10 +16,10 @@ class ChatViewController: UIViewController {
     
     let db = Firestore.firestore()
     var messages: [Message] = []
+    var shouldAnimate = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = K.appName
         navigationItem.hidesBackButton = true
         tableView.dataSource = self
         //        tableView.delegate = self
@@ -35,7 +35,13 @@ class ChatViewController: UIViewController {
                 K.FStore.senderField: sender,
                 K.FStore.bodyField: body,
                 K.FStore.dateField: Date().timeIntervalSince1970
-            ]) { (error) in if let e = error { print("There was an issue saving data to firestore: \(e)") } }
+            ]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to firestore: \(e)")
+                } else {
+                    DispatchQueue.main.async { self.messageTextfield.text = "" }
+                }
+            }
         }
     }
     
@@ -65,6 +71,9 @@ class ChatViewController: UIViewController {
                                 self.messages.append(message)
                                 DispatchQueue.main.async {
                                     self.tableView.reloadData()
+                                    let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                    self.tableView.scrollToRow(at: indexPath, at: .top, animated: self.shouldAnimate)
+                                    self.shouldAnimate = true
                                 }
                             }
                         }
@@ -82,8 +91,14 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        let isItMe = message.sender == Auth.auth().currentUser?.email
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label.text = messages[indexPath.row].body
+        cell.label.text = message.body
+        cell.leftImageView.isHidden = isItMe
+        cell.rightImageView.isHidden = !isItMe
+        cell.messageBubble.backgroundColor = UIColor(named: isItMe ? K.BrandColors.lightPurple : K.BrandColors.purple)
+        cell.label.textColor = UIColor(named: isItMe ? K.BrandColors.purple : K.BrandColors.lightPurple)
         return cell
     }
 }
